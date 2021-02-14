09b128aadd8138ed33e512a5be3f7a0d30b0d415
Valentin <valentin.colin78@gmail.com>
2021-02-14 17:23:03 +0100

    jail fonctionnelle

--- cogs/jail.py
+++ cogs/jail.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3.9
 """Gestion de la prison sur discord."""
+import yaml
 import discord
 from discord.ext import commands
 from config import emoji
 from tools.access import access
-from tools.format import fcite, fmarkdown
+from tools.format import fcite, fmarkdown, fcode
 
 
 class Jail(commands.Cog):
@@ -12,8 +13,6 @@ class Jail(commands.Cog):
 
     role_name = "Jail"
     channel_name = "jail"
-
-    prisoners = []  # l.append('a') l.removed('a')
 
     # ###### #
     # Events #
@@ -25,58 +24,108 @@ class Jail(commands.Cog):
         print("    Jail's Cog is ready.")
 
     # ######### #
-    # Commandes #
+    # Functions #
     # ######### #
 
-    def get_role(self, ctx: commands.Context):
+    @classmethod
+    def get_jail_role(cls, ctx: commands.Context):
         """Renvoie le rôle prisonnier."""
-        return discord.utils.get(ctx.guild.roles, name=Jail.role_name)
+        return discord.utils.get(ctx.guild.roles, name=cls.role_name)
 
-    def get_voice_channel(self, ctx: commands.Context):
+    @classmethod
+    def get_jail_voice_channel(cls, ctx: commands.Context):
         """Renvoie le channel vocal de la prison."""
-        return discord.utils.get(ctx.guild.voice_channels, name=Jail.channel_name)
+        return discord.utils.get(ctx.guild.voice_channels, name=cls.channel_name)
+
+    @staticmethod
+    def save_roles(ctx: commands.Context, prisoner: discord.Member):
+        """Sauvegarde les roles du prisonnier."""
+        id_server = str(ctx.guild.id)
+        prisoner_roles_id = [role.id for role in prisoner.roles]
+        with open("data/yaml/bot.yml", "r") as f:
+            data = yaml.load(f, Loader=yaml.FullLoader)
+        if id_server not in data["servers"]:
+            data["servers"][id_server] = data["servers"]["default"]
+
+        # Ajout du prisonnier et ses rôles à la liste des prisonniers,
+        # si il n'y es pas déjà.
+        data["servers"][id_server]["prisoners"][str(prisoner.id)] = prisoner_roles_id
+
+        with open("data/yaml/bot.yml", "w") as f:
+            yaml.dump(data, f)
+
+    @staticmethod
+    def get_roles(ctx: commands.Context, user: discord.Member):
+        """Récupère les anciens rôles du prisonnier.
+        Et supprime le prisonnier de la liste des prisonnier."""
+        id_server = str(ctx.guild.id)
+        with open("data/yaml/bot.yml", "r") as f:
+            data = yaml.load(f, Loader=yaml.FullLoader)
+        # Stock les rôles à renvoyer
+        user_roles_id = data["servers"][id_server]["prisoners"][str(user.id)]
+        # Supprime le prisonnier de la bdd
+        del data["servers"][id_server]["prisoners"][str(user.id)]
+        with open("data/yaml/bot.yml", "w") as f:
+            yaml.dump(data, f)
+        return user_roles_id
+
+    # ######### #
+    # Commandes #
+    # ######### #
 
     @commands.command(aliases=["emprisonner", "emprisonnement"])
     @access.has_role("Policeman")
     async def jail(self, ctx: commands.Context, new_prisoner: discord.Member):
         """Envoie une personne en prison."""
-        jail_role = self.get_role(ctx)
-        jail_voice_channel = self.get_voice_channel(ctx)
-        try:
-            await new_prisoner.edit(
-                mute=True,
-                deafen=False,
-                roles=[jail_role],
-                voice_channel=jail_voice_channel,
-                reason="Tu es à présent en prison !",
-            )
-        except:
-            # await new_prisoner.add_roles(jail_role)
-            # await new_prisoner.move_to(jail_voice_channel)
-            await new_prisoner.edit(mute=True, deafen=False, roles=[jail_role])
-        Jail.prisoners.append(new_prisoner)
-        txt = f"{emoji.lock} {new_prisoner.mention}, vous avez été placé en prison ! {emoji.lock}"
+        self.save_roles(ctx, new_prisoner)
+        minus_roles = new_prisoner.roles[1:]
+        plus_roles = [self.get_jail_role(ctx)]
+        dashboard_roles = "\n - ".join([""] + [role.name for role in minus_roles])
+        dashboard_roles += "\n + ".join([""] + [role.name for role in plus_roles])
+        await new_prisoner.edit(
+            mute=True,
+            deafen=False,
+            roles=[self.get_jail_role(ctx)],
+            voice_channel=self.get_jail_voice_channel(ctx),
+            reason="Tu es à présent en prison !",
+        )
+        await new_prisoner.move_to(self.get_jail_voice_channel(ctx))
+        txt = (
+            f"{emoji.lock} {new_prisoner.mention}, "
+            f"vous avez été placé en prison ! {emoji.lock}\n"
+            f"> Tes rôles ne t'appartiennent plus !"
+        )
         await ctx.send(fcite(txt))
+        await ctx.send(fcode(dashboard_roles, "md"))
 
     @commands.command(aliases=["liberer", "liberation"])
     @access.has_role("Policeman")
     async def unjail(self, ctx: commands.Context, new_free_user: discord.Member):
         """Libérer une personne de prison."""
-        jail_role = self.get_role(ctx)
-        try:
-            await new_free_user.edit(
-                mute=False,
-                deafen=False,
-                roles=[],
-                # voice_channel=None,
-                reason="Tu n'as plus besoins d'être dans le channel de la prison",
-            )
-        except:
-            await new_free_user.remove_roles(jail_role)
-            # await new_free_user.move_to(channel=None)
-        Jail.prisoners.remove(new_free_user)
-        txt = f"{emoji.unlock} {new_free_user.mention}, vous avez été libéré de prison ! {emoji.unlock}"
+        user_roles_id = self.get_roles(ctx, new_free_user)
+        user_roles = []
+        for role in ctx.guild.roles:
+            if role.id in user_roles_id:
+                user_roles.append(role)
+
+        minus_roles = [self.get_jail_role(ctx)]
+        plus_roles = [role for role in user_roles]
+        dashboard_roles = "\n - ".join([""] + [role.name for role in minus_roles])
+        dashboard_roles += "\n + ".join([""] + [role.name for role in plus_roles])
+
+        await new_free_user.edit(
+            mute=False,
+            deafen=False,
+            roles=user_roles,
+            reason="Tu es libéré de prison !",
+        )
+        txt = (
+            f"{emoji.unlock} {new_free_user.mention}, "
+            f"vous avez été libéré de prison ! {emoji.unlock}\n"
+            f"> Tu peux récupérer ce qui t'appartient."
+        )
         await ctx.send(fcite(txt))
+        await ctx.send(fcode(dashboard_roles, "md"))
 
     @commands.command(aliases=["voir_prisonnier"])
     async def jail_visitor(self, ctx: commands.Context):
